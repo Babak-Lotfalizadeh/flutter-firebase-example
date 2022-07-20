ab15e9e1ceb793936caf8fefe72e1f07cd86f894
babak.lotfalizadeh@geeks.ltd.uk <babak.lotfalizadeh@geeks.ltd.uk>
2022-07-20 20:10:37 +0430

    add text theme from google font

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeProvider extends ChangeNotifier {
   final ThemeData _themeData = ThemeData(
-    primaryColor: Colors.red,
+    primaryColor: Colors.blue,
+    textTheme: GoogleFonts.robotoTextTheme(),
+    primaryTextTheme: GoogleFonts.robotoTextTheme(),
   );
 
   ThemeData get themeData => _themeData;
