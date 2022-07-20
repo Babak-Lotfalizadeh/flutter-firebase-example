7d516bc6d8cc3f0206b465aff1308ec0c0241268
babak.lotfalizadeh@geeks.ltd.uk <babak.lotfalizadeh@geeks.ltd.uk>
2022-07-20 18:17:28 +0430

    implement logic for FirebaseAnalytics

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -1,12 +1,24 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_example/firebase_options.dart';
 
 class FirebaseServices {
+  static late FirebaseAnalytics analytics;
+
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await _initCore();
+    await _initAnalytic();
+  }
+
+  static Future<void> _initCore() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+  }
+
+  static Future<void> _initAnalytic() async {
+    analytics = FirebaseAnalytics.instance;
   }
 }
---
+++ lib/util/utility.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Utility {
+  static void print(String log) {
+    debugPrint(log);
+  }
+}
