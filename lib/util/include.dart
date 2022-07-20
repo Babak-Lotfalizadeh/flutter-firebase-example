35a575ee94905f04fbac821055c9c33cab7e7374
babak.lotfalizadeh@geeks.ltd.uk <babak.lotfalizadeh@geeks.ltd.uk>
2022-07-20 20:31:24 +0430

    move import to include

--- lib/util/include.dart
+++ lib/util/include.dart
@@ -1,8 +1,9 @@
 export 'package:flutter/material.dart';
 export 'package:flutter_firebase_example/config/secure.dart';
 export 'package:flutter_firebase_example/resources/keys_values.dart';
+export 'package:flutter_firebase_example/resources/screen_values.dart';
+export 'package:flutter_firebase_example/resources/screen_values.dart';
 export 'package:flutter_firebase_example/services/navigation_service.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:google_fonts/google_fonts.dart';
-export 'package:provider/provider.dart';
-export 'package:flutter_firebase_example/resources/screen_values.dart';
+export 'package:provider/providern_values.dart';
